127400c906ff0e4fa013e08a4c39edbce2d6fbad
Kaloyan Todorov <k.todorov@outlook.com>
2016-05-20 03:31:19 +0200

    Practica Basica

--- index.js
+++ index.js
@@ -17,13 +17,6 @@ $.fn.scrollTo = function( target, options, callback ){
 	});
 };
 
-last_scroll = 0 ;
-last_clicked = null ;
-last_cat_hotel_clicked = null ;
-last_cat_clicked = null ;
-full_hotels_info = null ;
-inserted_cat_list = false 
-
 // Hace scroll y selecciona el campo.
 // Se le pasa la casilla que se quiere seleccionar del div.
 scroll_and_check = function(number) {
@@ -39,17 +32,36 @@ scroll_and_check = function(number) {
     	last_clicked = $( hotel ) ;
 }
 
-var lista_categorias = {}
+// Variables globales. Para evitar pasar los datos como parametros a las funciones.
+last_scroll = 0 ;
+last_clicked = null ;
+last_cat_hotel_clicked = null ;
+last_cat_clicked = null ;
+full_hotels_info = null ;  // service array
+inserted_cat_list = false ;
+selected_hotel = 0 ;
+selected_hotel_name = "" ;
+current = "" ;
+inserted_repo_info_form = false ;
+got_git_file = false ;
+github = null ;
+git_info = {}
+data_to_save = false ;
+
+lista_alojados = {} 
+lista_categorias = {}
 
 // Codigo que se ejecuta cuando se carga el DOM
 $(function() {
-
+	// Inserta mapa
 	var madrid_coords = [40.433333, -3.683333]
 	var map = L.map('map').setView( madrid_coords, 13);
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(map);
 
-	var current = "#mapa" // Inicializo la primera pestana
+	current = "#mapa" // Inicializo la primera pestana
 	$(current).addClass("active")
+
+	full_hotels_info  = insertHotels(map) // Una vez cargado el DOM inserta los hoteles.
 
 	// Evento para las animaciones cuando se hace click sobre la pestana colecciones.
 	$("#colecciones").click( function() {
@@ -86,6 +98,7 @@ $(function() {
 	})
 
 	$("#alojados").click( function() {
+		show_hotel()
 
 		if ( current == "#colecciones" )
 			alojados_click_from_collection()
@@ -98,28 +111,25 @@ $(function() {
 		$(current).addClass("active")
 	})
 
-	full_hotels_info  = insertHotels(map) // Una vez cargado el DOM inserta los hoteles.
-
 	// Eventos para cambiar el boton de crear categoria.
-	$("#cat_input").focusin(function() {
-		$("#cat_button").addClass("btn-primary").addClass("button_focused").removeClass("btn-default")
+	$("#cat_git_input").focusin(function() {
+		$("#cat_git_button").addClass("btn-primary").addClass("button_focused").removeClass("btn-default")
 	})
 
-	$("#cat_input").focusout(function() {
-		$("#cat_button").addClass("btn-default").removeClass("button_focused").removeClass("btn-primary");
+	$("#cat_git_input").focusout(function() {
+		$("#cat_git_button").addClass("btn-default").removeClass("button_focused").removeClass("btn-primary");
 
 	})
 
-	$('#cat_input').keyup( function (e) {
+	// Creando las categorias.
+	$('#cat_git_input').keyup( function (e) {
 		if (e.keyCode === 13) {
-			crear_categoria()
+			get_token()
 		}
 	})
 
-	$("#cat_button").click(crear_categoria)
-
+	$("#cat_git_button").click(get_token)
 });
-
 
 // Funcion que me inserta la lista de hoteles desde el json.
 // Ademas dado el mapa me los muestra en este.
@@ -127,20 +137,26 @@ $(function() {
 // y asi no tener que pedirlos otra vez.
 var insertHotels = function(map) {
 	var hotel_list
+	var result 
 	$.ajax({
 		dataType: "json",
 		method: "GET",
+		async: false,
 		url: "https://raw.githubusercontent.com/CursosWeb/Code/master/JS-APIs/misc/alojamientos/alojamientos.json",
+            	success: function(data) {
+                	result = data;
+            	}
 	})
 		.done(function( msg ) {
 			hotel_list = msg.serviceList.service 
+			selected_hotel_name = hotel_list[selected_hotel].basicData.name
 			dom_hotel_list = $("#hotel_list")
 			aux_hotel_list = ""
 			for (var i = 0 ; i < hotel_list.length ; i++){
 				aux_hotel_list += "<div class=' hotel ' value='" + i + "'" 
 				aux_hotel_list += "name='" +  hotel_list[i].basicData.name  + " '>"
 				aux_hotel_list += "<p>" + hotel_list[i].basicData.name + "</p></div>"
-				hotel_list[i].index = i 
+				hotel_list[i].index = i // hotels index, para poder localizarlo.
 			}
 			dom_hotel_list.append(aux_hotel_list)
 			hotel_list = insertHotels_Markers(hotel_list, map)
@@ -149,15 +165,21 @@ var insertHotels = function(map) {
 			$(".hotel").draggable({
 				helper: "clone"
 			}).click( function() {
-				var i = parseInt($(this).attr("value"))
+
+				var i = parseInt($(this).attr("value")) // hotels index
+
+				selected_hotel = i
+				selected_hotel_name = $(this).attr("name")
+				
 				hotel_list[i].marker.openPopup()
 				scroll_and_check(i)
 				map.setView( [hotel_list[i].geoData.latitude , hotel_list[i].geoData.longitude ], 18)
 
-			});
-	});
+			})
 
-	return hotel_list 
+		})
+
+	return result.serviceList.service
 }
 
 // Dada la lista de hoteles y el mapa, crea los markers ( con sus respectivos
@@ -167,12 +189,13 @@ var insertHotels_Markers = function(list , map) {
 	for (var i = 0 ; i < list.length ; i++){
 		list[i].marker = L.marker( [list[i].geoData.latitude , list[i].geoData.longitude ] )
 
-		var elto = "<p>" + list[i].basicData.name + "<br>" 
+		var elto = "<p> <a onclick='trigger_alojados(this)' href='#'>" + list[i].basicData.name + "</a><br>" 
 		elto += "<b>Address: </b> " + list[i].geoData.address + "</p>"
 
 		list[i].marker.addTo(map).bindPopup(elto)
 		list[i].marker.index = i
 		list[i].marker.on("click" , function() {
+			selected_hotel = this.index 
 			scroll_and_check(this.index)
 		})
 	}
@@ -180,6 +203,9 @@ var insertHotels_Markers = function(list , map) {
 	return list 
 }
 
+trigger_alojados = function(ev) {
+	$("#alojados").trigger("click")
+}
 
 // Funcion que realiza las animaciones para pasar a la pestana de colecciones
 // desde la pestana de mapa.
@@ -197,26 +223,27 @@ var collection_click_from_map = function (inserted_cat_list) {
 		height : "800px"
 	}, 250 )
 
-	if (!inserted_cat_list) { // La primera vez compirmira los dos div iniciales
+	if (!inserted_cat_list && got_git_file  ) { // La primera vez compirmira los dos div iniciales
 				  // e insertara el de categorie_hotels
 
-	$("#hotel_list").animate({
-		width : "32%"  
-	}, 250, "linear" , function() {
+		$("#hotel_list").parent().removeClass("col-lg-6")	
+		$("#hotel_list").parent().addClass("col-lg-4")
+		$("#hotel_list").parent().removeClass("col-md-6")
+		$("#hotel_list").parent().addClass("col-md-4")
+		$("#hotel_list").parent().removeClass("col-sm-6")
+		$("#hotel_list").parent().addClass("col-sm-4")
 
-	$("#categories_index").animate({
-		left : $("#categories_index").width(),
-		width : "32%"
-	}, 250, "linear" , function() {
+		$("#categories_index").parent().removeClass("col-lg-6")
+		$("#categories_index").parent().addClass("col-lg-4")
+		$("#categories_index").parent().removeClass("col-md-6")
+		$("#categories_index").parent().addClass("col-md-4")
+		$("#categories_index").parent().removeClass("col-sm-6")
+		$("#categories_index").parent().addClass("col-sm-4")
 
-	cat_hotels = "<div id='categorie_hotels' style='height : 800px ; margin-top : 20px;'></div>" 
-	$("#collections").append(cat_hotels)
+		cat_hotels = "<div class='col-lg-4 col-md-4 col-sm-4 col-xs-12' ><div id='categorie_hotels'  style='height : 800px ; margin-top : 20px;'></div></div>" 
+		$("#collections").append(cat_hotels)
 
-	$("#categorie_hotels").animate({
-		width: "32%"
-	}, 250 )
-
-	})})
+	}else {
 
 	}
 
@@ -225,10 +252,11 @@ var collection_click_from_map = function (inserted_cat_list) {
 	}, 250 )
 
 	return true ; // Devuelve una afirmacion de que ha sido llamado.
-
 }
 
 var collection_click_from_hotel_info = function(aux_var, to_map, to_hotel) {
+
+    	$("#hotel_info").empty()
 
     	$("#hotel_info").animate({
     		height : "0px"  
@@ -272,7 +300,7 @@ var map_click_from_collection = function() {
 		marginTop: "20px",
 		height : "294px"
 	}, 250 )
-	
+
 	$("#map").animate({
 	    	height: "500px"
     	}, 250)
@@ -314,9 +342,17 @@ var alojados_click_from_map = function() {
 	alojados_click_from_collection()
 }
 
+var insertar_categorias = function() {
+
+	for (cat in lista_categorias ){
+		console.log(lista_categorias[cat])
+		crear_categoria(null , cat, lista_categorias[cat] )
+	} 
+}
+
 // Funcion para crear una categoria nueva
 // Añade todas las opciones para que le puedan ser añadidos hoteles.
-var crear_categoria = function(that,new_cat_name) {
+var crear_categoria = function(that,new_cat_name , hotels) {
 
 	if(!new_cat_name){
 		new_cat_name = $("#cat_input").val()
@@ -327,7 +363,11 @@ var crear_categoria = function(that,new_cat_name) {
 
 	$("#categories_index").append(new_cat)
 
-	lista_categorias[new_cat_name] = new Array()  
+	if(hotels){
+		lista_categorias[new_cat_name] = hotels
+	} else {
+		lista_categorias = new Array()
+	}
 
 	// Programa los eventos de droppable para que puedan ser arrastrados elementos en la categoria
 	$( ".categoria[value='" + new_cat_name + "']" ).droppable({
@@ -357,8 +397,11 @@ var crear_categoria = function(that,new_cat_name) {
 
 	})
  
-	return new_cat_name
+        if(!data_to_save)
+        	insert_save_button()
 
+
+	return new_cat_name
 }
 
 // Inserta los hoteles pertenecientes a la categoria dada en el ultimo div categorie_hotels
@@ -386,7 +429,304 @@ var insert_cat_hotels = function(cat) {
 	}
 }
 
+// Dado el nombre del hotel devuelve la informacion completa del hotel.
+var get_hotel_by_name = function(hotel) {
+
+	for ( var i = 0 ; i < full_hotels_info.length ; i++ ) {
+		if ( full_hotels_info[i].basicData.name == hotel  )
+			return full_hotels_info[i]
+	}
+
+	return null
+}
+
+// Rellena la pestaña de alojados con la informacion de hotel seleccionado.
 var show_hotel = function(hotel) {
 
+	var my_hotel = full_hotels_info[selected_hotel]
+	var media = my_hotel.multimedia
 
+	// Titulo
+	$("#hotel_info").append('<div id="title"></div>')
+	$("#title").empty()
+	$("#title").append("<h1><b>" + my_hotel.basicData.name +  "</b></h1>")
+
+	// Carousel.	
+	if (media)
+		insert_carousel(media)
+
+	// Descripcion
+	var des = "<p>" + my_hotel.basicData.body + "</p>"
+	des  += "<p> <b> Web: </b> <a href='" + my_hotel.basicData.web + "'>" + my_hotel.basicData.web + "</a></p>"
+	des  += "<p> <b> Teléfono: </b> " + my_hotel.basicData.phone + "</p>"
+	des  += "<p> <b> Dirección: </b> " + my_hotel.geoData.address +"  ("+ my_hotel.geoData.subAdministrativeArea  + ")"+ "</p>"
+
+	$("#hotel_info").append('<div id="hotel_info_des"></div>')
+	$("#hotel_info_des").empty()
+	$("#hotel_info_des").append(des)
+
+
+	if ( got_git_file) { // Inserta la lista de alojados solo si he cargado el fichero de github.
+		// Lista de alojados.
+		$("#hotel_info").append('<div id="hotel_info_users_list"></div>')
+
+			// Formulario para añadir nuevos alojados.
+		var form = "<h3 style='text-align: center '><b> Lista de Alojados </b></h3>"
+	        form += '<div class="input-group" >'
+	        form +=        '<input id="user_input" type="text" class="form-control" placeholder="Añadir nuevo alojado...">'
+	        form +=        '<span class="input-group-btn">'
+	        form +=                '<button id="alojado_button" class="btn btn-default" type="button"><b>Añadir</b></button>'
+	        form +=        '</span>'
+	        form +=  '</div><!-- /input-group -->'
+	       // form +=		'<input id="comment_input" type="text" class="form-control" placeholder="Deje su comentario...">'
+
+	        $("#hotel_info_users_list").append('<div id="hotel_info_user_form"></div> ')
+	        $("#hotel_info_user_form").append(form)
+
+		$("#user_input").focusin(function() {
+			$("#alojado_button").addClass("btn-primary").addClass("button_focused").removeClass("btn-default")
+		})
+
+		$("#user_input").focusout(function() {
+			$("#alojado_button").addClass("btn-default").removeClass("button_focused").removeClass("btn-primary");
+
+		})
+
+		gapi.client.setApiKey('AIzaSyA_cdGsOl0l_mciZRao_oQhE7lNtGX-4Cs');
+		// INsertando los alojados
+		var user_list = lista_alojados[selected_hotel_name]
+		insert_user_list(user_list) 
+
+		$('#user_input').keyup( function (e) {
+			if (e.keyCode === 13) {
+				insert_user($("#user_input").val())
+				$("#user_input").val("") 
+			}
+		})
+
+		$("#alojado_button").click( function() {
+			insert_user( $("#user_input").val() )
+			$("#user_input").val("") 
+		})
+
+	}
+
+	// div para compsensar los margenes
+	$("#hotel_info").append('<div style="height : 70px" ></div>')
+
+	$("#hotel_info").show()
+
+}
+
+// Crea el carousel y lo inserta en el lugar donde tiene que ir.
+var insert_carousel = function(media) {
+	if(!media.media)
+		return null 
+	
+	$("#hotel_info").append('<div id="hotel_info_galery"></div>')
+	$("#hotel_info_galery").append('<div id="myCarousel" class="carousel slide" data-ride="carousel"></div>')
+	$("#myCarousel").empty()
+	$("#myCarousel").append('<div class="carousel-inner" role="listbox"> </div ')
+	$("#myCarousel").append('<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"> ' +
+                                        '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>' +
+                                        '<span class="sr-only">Previous</span>' +
+                                '</a>' +
+                                '<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">' +
+                                        '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>' +
+                                        '<span class="sr-only">Next</span>' +
+                                '</a>')
+
+	if(!media.media.length){
+		$(".carousel-inner").append("<div class='item active'>" +
+						"<img src='" + media.media.url + "'>" +
+						"</div")
+		return
+	}
+
+	var photos = ("<div class='item active'>" +
+				"<img src='" + media.media[0].url + "'>" +
+			"</div>")
+	for (var i = 1 ; i < media.media.length ; i++) {
+		photos += ("<div class='item'>" +
+				"<img src='" + media.media[i].url + "'>" +
+			"</div>")
+	}
+
+	$(".carousel-inner").append(photos)
+}
+
+var insert_user_list = function(user_list) {
+	// La api va a pelo, no debería, pero no se especifica nada en el enunciado.
+	// Si no habria que pedirla como se hace con el token de github.
+	if (!user_list)
+		return
+
+        for ( var i = 0 ; i < user_list.length ; i++) {
+        	insert_user(user_list[i])
+        }
+}
+
+var insert_user = function(user) {
+
+	if(!user)
+		return
+
+	if(!lista_alojados[selected_hotel_name]){
+		lista_alojados[selected_hotel_name] = new Array()
+	}
+
+	if(lista_alojados[selected_hotel_name].indexOf(user)==-1){
+		lista_alojados[selected_hotel_name].push(user)
+	}else if (current == "#alojados" ){
+		return
+	}
+
+        gapi.client.load('plus', 'v1', function() {
+        	var request = gapi.client.plus.people.get({
+        		'userId': user 
+        		// 'userId': '+GregorioRobles'
+        	})
+        	request.execute(function(resp) {
+        		var heading = document.createElement('h4');
+        		heading.setAttribute("value" , user)
+        		var image = document.createElement('img');
+        		image.src = resp.image.url;
+
+        		var rem_image = document.createElement('img');
+        		rem_image.src = ("images/recycle-bin.png")
+        		rem_image.className = ("user_rem")
+
+        		heading.appendChild(image);
+        		heading.appendChild(document.createTextNode("   " + resp.displayName));
+        		heading.appendChild(rem_image)
+
+        		// Evento para poder borrar el alojado.
+        		rem_image.addEventListener("click", function(){
+        			var val =  $(this.parentNode)[0].attributes[0].value
+        			for( var i = 0 ; i < lista_alojados[selected_hotel_name].length ; i++ ) {
+        				if (lista_alojados[selected_hotel_name][i] == val ) {
+        					delete lista_alojados[selected_hotel_name][i] 
+        				}
+        			}
+
+    				$(this.parentNode).hide()
+			});
+
+        		document.getElementById('hotel_info_users_list').appendChild(heading);
+        	})
+        })
+
+        if(!data_to_save)
+        	insert_save_button()
+}
+
+var insert_cat_input = function(into_div) {
+
+        var form = '<div class="input_form">'
+        form +=         '<div class="input-group" style="height : 0px " >'
+        form +=                        '<input id="cat_input" type="text" class="form-control" placeholder="Nueva colección...">'
+        form +=                 '<span class="input-group-btn">'
+        form +=                         '<button id="cat_button" class="btn btn-default" type="button"><b>Crear</b></button>'
+        form +=                 '</span>'
+        form +=         '</div><!-- /input-group -->'
+        form += '</div>'
+
+        $(into_div).append(form)
+
+	// Eventos para cambiar el boton de crear categoria.
+	$("#cat_input").focusin(function() {
+		$("#cat_button").addClass("btn-primary").addClass("button_focused").removeClass("btn-default")
+	})
+
+	$("#cat_input").focusout(function() {
+		$("#cat_button").addClass("btn-default").removeClass("button_focused").removeClass("btn-primary");
+
+	})
+
+	// Creando las categorias.
+	$('#cat_input').keyup( function (e) {
+		if (e.keyCode === 13) {
+			crear_categoria()
+		}
+	})
+
+	$("#cat_button").click(crear_categoria)
+}
+
+var get_token = function() {
+	var token = $("#cat_git_input").val()
+	github = new Github({
+		token: token,
+		auth: "oauth"
+    	})
+
+	insert_repo_info()
+}
+
+// Formulario para recoger los datos del fichero de github.
+var insert_repo_info = function() {
+
+	inserted_repo_info_form = true ;
+
+        var form = '<div class="input_form" id="repo_info">'
+        form +=         '<div class="input-group" >'
+        form +=                 '<input id="cat_git_nick_input" type="text" class="form-control" placeholder="Github nick...">'
+        form += 		'<input id="cat_git_repo_input" type="text" class="form-control" placeholder="Repo Name ...">'
+        form += 		'<input id="cat_git_file_input" type="text" class="form-control" style="width : 34%" placeholder="File Name...">'
+        form +=                 '<span class="input-group-btn">'
+        form +=                         '<button id="cat_git2_button" class="btn btn-default" type="button" ><b>Leer</b></button>'
+        form +=                 '</span>'
+        form +=         '</div><!-- /input-group -->'
+        form += '</div>'
+
+        $("#categories_index").append(form)
+        $("#token_form").hide() 
+
+        $("#cat_git2_button").click(get_file_info)
+}
+
+var get_file_info = function() {
+
+	var nick = $("#cat_git_nick_input").val() 
+	var repo = $("#cat_git_repo_input").val()
+	var file = $("#cat_git_file_input").val()
+
+	git_info["nick"] = nick
+	git_info["repo"] = repo
+	git_info["file"] = file
+
+	myrepo = github.getRepo(nick, repo)
+
+	myrepo.read('master', file, function(err, data) {
+		var info = JSON.parse(data)
+		lista_categorias = info["categorias"]
+		lista_alojados = info["alojados"]
+		insertar_categorias()
+    	});
+
+
+	var into_div = $("#categories_index")
+	into_div.empty()
+	insert_cat_input(into_div)
+
+	got_git_file = true ;
+}
+
+var insert_save_button = function() {
+	data_to_save = true 
+        var button =  '<button id="save_button" class="btn btn-primary" type="button" ><b>Guardar Cambios</b></button>'
+        $("#header>.container>.navbar-header").append(button)
+
+        $("#save_button").click(function() {
+
+        	var info_to_save = { "categorias" : lista_categorias , "alojados" : lista_alojados }
+
+        	myrepo.write(
+        		'master',				// rama
+        		git_info["file"], 			// fichero
+			JSON.stringify ( info_to_save ),	// contenido a escribir
+			"Updating data", function(err) {	// call back
+			     console.log (err)
+		});
+        })
 }
